a3bd4fdbd68d725b7cbfe7933fde0f4adbe9bfc6
Basil Kucherov <basilikgreat@gmail.com>
2025-11-19 22:08:26 +0300

    Add cdfsl

---
+++ fsl/cdfsl/configs.py
@@ -0,0 +1,9 @@
+save_dir                    = './logs'
+
+miniImageNet_path           	= '/ssd/miniImagenet'
+DTD_path 						= '/ssd/dtd/images/'
+
+ISIC_path = "/ssd/data/ISIC"
+ChestX_path = "/ssd/data/chestX"
+CropDisease_path = "/ssd/data/CropDiseases"
+EuroSAT_path = "/ssd/data/EuroSAT/2750"
